2a65c6e7bc8e146a04c314fd3594874c9ed19efc
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-29 04:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,3 +38,15 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
     });
   });
   */
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+     
+function johnLennonFacts() {
+
+     var newFacts = [];
+     var i = 0;
+     while (i < facts.length) {
+         newFacts.push(facts[i] + "!!!");
+         i++;
+     }
+     return newFacts;
+ }  
