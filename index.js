e7ad3d88f2707019e03932a7fe47d4d97c7086e2
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-29 03:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]; 
 
 theBeatlesPlay(musicians, instruments);
+  
+function theBeatlesPlay(musicians, instruments) {
+  
+     var allMusicians = [];
+     
+     for (var i=0; i < musicians.length; i++) {
+         allMusicians.push(musicians[i]  + " plays " + instruments[i]);
+     }
+     return allMusicians;
+ }
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]; 
+
+theBeatlesPlay(musicians, instruments);
