f08000f21c8ecdce75fb2e6ca65a28233b960455
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-29 03:59:06 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -33,3 +33,33 @@ describe('Beatles Loops', function() {
   });
 
 });
+/*  describe('johnLennonFacts', function(){
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
+  */
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+     
+function johnLennonFacts() {
+
+     var newFacts = [];
+     var i = 0;
+     while (i < facts.length) {
+         newFacts.push(facts[i] + "!!!");
+         i++;
+     }
+     return newFacts;
+ }  
+// johnLennonFacts();
+ 
