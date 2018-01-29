a704aaccdd9be898b44cc0d370bfa8333e492d5d
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-29 04:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments) {
      return allMusicians;
  }
 
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
      
 function johnLennonFacts() {
 
